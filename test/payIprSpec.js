6192dae1cd61a00af2bd4aedbfddbf33a593a1e0
Ben Sharafian <benms64@gmail.com>
2017-06-05 11:46:34 -0700

    test: test payIpr

--- test/payIprSpec.js
+++ test/payIprSpec.js
@@ -22,7 +22,9 @@ describe('/payIpr', () => {
     this.ipr = utils.base64url(ILP.IPR.createIPR({
       destinationAmount: '1000',
       destinationAccount: 'example.blue.bob',
-      receiverSecret: Buffer.from('secret')
+      receiverSecret: Buffer.from('secret'),
+      publicHeaders: { 'Payment-Id': 'id_of_payment' },
+      disableEncryption: true
     }))
     this.ctx.request.body = {
       ipr: this.ipr,
@@ -59,5 +61,58 @@ describe('/payIpr', () => {
     await assert.isRejected(
       payIpr(this.config, this.factory, this.ctx),
       /400.*missing JSON body field connectorAccount/)
+  })
+
+  it('should submit a payment based on the IPR', async function () {
+    this.factory.plugin.sendTransfer = (transfer) => {
+      setImmediate(() => {
+        this.factory.plugin.emit('outgoing_fulfill', transfer, 'fulfillment')
+      })
+      return Promise.resolve()
+    }
+
+    await payIpr(this.config, this.factory, this.ctx)
+    assert.deepEqual(this.ctx.body, {
+      connectorAccount: 'example.red.connie',
+      paymentId: 'id_of_payment',
+      status: 'executed',
+      fulfillment: 'fulfillment'
+    })
+  })
+
+  it('should handle payment rejection', async function () {
+    this.factory.plugin.sendTransfer = (transfer) => {
+      setImmediate(() => {
+        this.factory.plugin.emit('outgoing_reject', transfer,
+          { reason: 'rejected' })
+      })
+      return Promise.resolve()
+    }
+
+    await payIpr(this.config, this.factory, this.ctx)
+    assert.deepEqual(this.ctx.body, {
+      connectorAccount: 'example.red.connie',
+      paymentId: 'id_of_payment',
+      status: 'rejected',
+      rejectionMessage: {
+        reason: 'rejected'
+      }
+    })
+  })
+
+  it('should handle payment expiration', async function () {
+    this.factory.plugin.sendTransfer = (transfer) => {
+      setImmediate(() => {
+        this.factory.plugin.emit('outgoing_cancel', transfer)
+      })
+      return Promise.resolve()
+    }
+
+    await payIpr(this.config, this.factory, this.ctx)
+    assert.deepEqual(this.ctx.body, {
+      connectorAccount: 'example.red.connie',
+      paymentId: 'id_of_payment',
+      status: 'expired'
+    })
   })
 })
